1b769cec01f658a01e25900d8afa449467f74e8d
Cholponai <111632987+Cholponai02@users.noreply.github.com>
2025-05-25 09:59:46 +0600

    Add files via upload

---
+++ PythonProject4/tkinterConverter.py
@@ -0,0 +1,73 @@
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⢦⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⡀⠈⣇⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢘⣷⣄⣙⡏⠁⠀⠀⢬⣉⠙⠶⣄⣀⣀⣀⣠⣤⣤⡄⠀⠀⠀
+# ⠀⠀⠀⠀⠀⢀⣼⠿⠟⠉⠉⠀⠀⠀⠀⠀⠉⠙⠛⢿⠉⠉⠁⠀⠀⢸⡇⠀⠀⠀
+# ⠀⠀⠀⣀⣶⡿⠁⠀⠀⠀⠀⡄⠀⢰⣆⠀⠀⠀⠀⠘⣇⠀⠀⠀⢀⣸⡇⠀⠀⠀
+# ⢴⠞⠛⠉⢹⡄⢰⡆⠀⠀⢸⢿⡆⠸⡟⣧⠀⠀⠀⠀⢹⡤⠶⠛⠛⣿⡅⠀⠀⠀
+# ⠸⣇⠀⠀⠸⡇⢸⠀⠀⣶⣿⠤⠿⣆⣿⠙⣳⣄⠀⠀⠈⣧⠀⠀⣺⣽⠇⠀⠀⠀
+# ⠀⠙⣶⣶⠿⡇⣾⡇⠀⣿⣿⣶⡄⠙⢿⡄⠏⠻⣷⣄⠀⢻⡶⢿⣿⠿⠂⠀⠀⠀
+# ⠀⠀⠹⡆⠀⣿⠋⢷⡀⣿⡟⠇⠉⠀⠀⠁⠀⠀⢻⡼⣿⠿⠷⣾⣅⡀⠀⠀⠀⠀
+# ⠀⠀⠀⠹⣾⣷⡶⠾⢷⣸⣷⡀⠀⠀⣆⣤⣴⣤⣾⡿⣏⣀⠀⢻⡿⠁⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⢹⣧⣄⣤⠿⣿⣿⣿⢶⣶⣾⣯⣿⢿⠁⣸⠏⠙⠛⠶⣄⡀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⢠⣾⣿⠷⠈⢁⣿⣄⣻⣿⣹⣿⣼⣸⣧⠀⠀⠀⠀⢈⣷⠀⠀⠀
+# ⠀⠀⠀⠀⣠⣶⠟⠋⠈⢳⣤⣾⡥⢽⡏⣿⠉⣸⡿⢯⣿⢷⣼⠃⠀⢸⡇⠀⠀⠀
+# ⠀⠀⠀⠀⠻⣏⠀⠀⠀⣾⠁⢻⡟⠋⣿⣿⠀⣿⣀⣤⣿⣼⠛⠇⢀⡿⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠘⣇⠀⠀⠈⢷⣿⠟⠛⣻⡟⢀⡟⠄⢀⣿⡇⠀⠀⢸⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⢀⣸⣧⠀⠀⠀⣿⠆⢰⣿⢢⣼⠃⠀⣾⢿⡀⠀⠀⣼⣿⣷⢲⣦⡄
+# ⠀⠀⠀⠐⣏⢻⣭⣿⡇⠀⣀⡿⠀⢸⣧⡾⣿⡀⠀⢡⣾⠻⢿⣴⣿⣿⣣⣾⠿⠀
+# ⠀⠀⠀⠀⠹⣄⠻⠿⣣⣴⠿⠶⠶⡿⠉⣇⠈⢳⣤⠿⣿⣶⡟⠹⣿⠛⠋⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠹⢷⣾⣿⡟⠛⠶⢼⣇⣀⣿⣠⡴⢿⡖⠛⢯⣙⡾⠉⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠈⣿⣿⠷⠶⢤⣼⣇⣠⣽⣇⣠⣤⣷⡴⠞⢿⡄⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⣿⡆⠀⠀⠀⠉⠙⣷⠈⢷⣀⣤⠾⣶⣶⣾⣿⡄⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠘⣿⡤⡴⠒⣛⣛⣿⣄⣈⣻⡾⢋⠉⠀⠀⢹⣿⠀⠀⠀⠀⠀
+
+
+import tkinter as tk
+from random import choice
+
+eng_layout = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,./";
+rus_layout = "йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.";
+
+window = tk.Tk()
+window.title("Переключатль раскладки")
+window.geometry("400x300")
+
+enter = tk.Entry(window, width=50)
+enter.pack(pady=10)
+
+var = tk.IntVar()
+rb1 = tk.Radiobutton(window, text="С Русского на английский", variable=var, value=1)
+rb1.pack()
+rb2 = tk.Radiobutton(window, text="С английского на Русский", variable=var, value=2)
+rb2.pack()
+rb1.select()
+
+def converter_Symbols():
+    print("Мой будущий ответ")
+    text = enter.get()
+    choice = var.get()
+    if choice == 1:
+        result = translater(text, eng_layout, rus_layout)
+    else:
+        result = translater(text, rus_layout, eng_layout)
+    result_label.config(text="Результат: " + result)
+
+def translater(text, from_layout, to_layout):
+    result = ""
+    for ch in text:
+        index = from_layout.find(ch)
+        if index != -1:
+            result = result + to_layout[index]
+        else:
+            result = result + ch
+    return result
+
+
+btn = tk.Button(window, text="Результат", command=converter_Symbols)
+btn.pack()
+
+result_label = tk.Label(window,text="Результат: ")
+result_label.pack()
+
+window.mainloop()
